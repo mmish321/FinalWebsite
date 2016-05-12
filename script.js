7d82a49a961fcc5faa3173e86630ea71148b7322
Michelle Mao <mm321whatsup@gmail.com>
2016-05-11 20:46:14 -0600

    Get looping done

--- script.js
+++ script.js
@@ -1,14 +1,37 @@
 var pokemon = new XMLHttpRequest();
-var url = "http://pokeapi.co/api/v2/evolution-chain/3";
+
+//var name = prompt("Whats the name of the pokemon you have?");
+var url = "http://pokeapi.co/api/v2/evolution-chain/33";
+ //  findName = function(name) {
+ //      var count = 1;
+ // 	while (count > 0) {
+ //  		var pokemon2 = new XMLHttpRequest();
+ // 		var url2 = "http://pokeapi.co/api/v2/evolution-chain/" + count;
+ //  		if (pokemon2.status == 400) {
+ //  			return false;
+ //  		}
+ //        var arr = JSON.parse(pokemon2.responseText);
+ //        if (arr.chain.species.name == name) {
+ //        	return count; 
+ //        }
+ //        if (arr.chain.evolves_to[0].species.name == name) {
+ //        	return count;
+ //        }
+ 		
+ //  		process the return to find if the name exists in the chain then break
+ //  		count++
+ 		
+ //  	}
+
+ // }
 
 pokemon.onreadystatechange = function(){
-	console.log(pokemon.readyState);
-	console.log(pokemon.status);
 	if(pokemon.readyState == 4 && pokemon.status == 200){
 		var myArr = JSON.parse(pokemon.responseText);
 		console.log(myArr);
-		console.log(myArr.id);
+		console.log(myArr.chain.species.name);
 		console.log(myArr.chain.evolves_to[0].species.name);
+		console.log(myArr.chain.evolves_to[1].species.name);
 		myFunction(myArr);
 	}
 }
@@ -20,7 +43,7 @@ function myFunction(arr){
 	var out  = "";
 	var i;
 	for(i = 0; i < arr.length; i++){
-		out += array[i].url;
+		out += array.chain.evolves_to[i].species.name;
 		console.log(out);
 	}
 }
